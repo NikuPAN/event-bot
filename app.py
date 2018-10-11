671d48721179a59209d12025f3db2f269f2e667d
Nick Pang <nick95927@gmail.com>
2018-10-11 15:49:26 +1000

    new cmd timezone FIX

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ handler = WebhookHandler('9ffa9b07f9a2dfef20cffd300af6df4e')
 
 # global variables
 app_name = '髮落士報時'
-version = 49
+version = 51
 mode = 1
 has_said = 0
 fmt = '%H:%M'
@@ -44,7 +44,7 @@ class myThread (td.Thread):
       self.event = event
 
    def run(self):
-      onPlayerTalk(self.user_message, self.event)	
+      onPlayerTalk(self.user_message, self.event)    
 
 # 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
@@ -64,111 +64,111 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-	global app_name, version, mode
-	user_message = event.message.text.lower()
-	# console command
-	if(user_message == "test"):
-		message = TextSendMessage(text='歡迎使用'+app_name+' v1.'+str(version)+'！')
-		line_bot_api.reply_message(event.reply_token,message)
-	elif(user_message == "/stop"):
-		message = TextSendMessage(text=app_name+'已被關閉！')
-		line_bot_api.reply_message(event.reply_token,message)
-		mode = 0
-	elif(user_message == "/start"):
-		message = TextSendMessage(text=app_name+'已經開啟！')
-		line_bot_api.reply_message(event.reply_token,message)
-		mode = 1	
-	elif(user_message == "/help"):
-		usage(event)
-	# other command using thread
-	else:
-		myThread(user_message, event).start()
+    global app_name, version, mode
+    user_message = event.message.text.lower()
+    # console command
+    if(user_message == "test"):
+        message = TextSendMessage(text='歡迎使用'+app_name+' v1.'+str(version)+'！')
+        line_bot_api.reply_message(event.reply_token,message)
+    elif(user_message == "/stop"):
+        message = TextSendMessage(text=app_name+'已被關閉！')
+        line_bot_api.reply_message(event.reply_token,message)
+        mode = 0
+    elif(user_message == "/start"):
+        message = TextSendMessage(text=app_name+'已經開啟！')
+        line_bot_api.reply_message(event.reply_token,message)
+        mode = 1    
+    elif(user_message == "/help"):
+        usage(event)
+    # other command using thread
+    else:
+        myThread(user_message, event).start()
 
 def usage(event):
-	global usage_cmd
-	message = TextSendMessage(text=usage_cmd)
-	line_bot_api.reply_message(event.reply_token,message)
-	
+    global usage_cmd
+    message = TextSendMessage(text=usage_cmd)
+    line_bot_api.reply_message(event.reply_token,message)
+    
 def hour_Convert(Hour):
-	if(Hour >= 0 and Hour <= 3):
-		return "凌晨 " + str(Hour)
-	elif(Hour >= 4 and Hour <= 6):
-		return "清晨 " + str(Hour)
-	elif(Hour >= 7 and Hour <= 11):
-		return "早上 " + str(Hour)
-	elif(Hour == 12):
-		return "中午 " + str(Hour)
-	elif(Hour >= 13 and Hour <= 18):
-		return "下午 " + str(Hour - 12)
-	elif(Hour >= 19 and Hour <= 23):
-		return "晚上 " + str(Hour - 12)
+    if(Hour >= 0 and Hour <= 3):
+        return "凌晨 " + str(Hour)
+    elif(Hour >= 4 and Hour <= 6):
+        return "清晨 " + str(Hour)
+    elif(Hour >= 7 and Hour <= 11):
+        return "早上 " + str(Hour)
+    elif(Hour == 12):
+        return "中午 " + str(Hour)
+    elif(Hour >= 13 and Hour <= 18):
+        return "下午 " + str(Hour - 12)
+    elif(Hour >= 19 and Hour <= 23):
+        return "晚上 " + str(Hour - 12)
 
 def set_timeRegion_TW():
-	global systime, sysregion
-	systime = timezone('Asia/Taipei')
-	sysregion = 'TW'
-	return "台北"
+    global systime, sysregion
+    systime = timezone('Asia/Taipei')
+    sysregion = 'TW'
+    return "台北"
 
 def set_timeRegion_JP():
-	global systime, sysregion
-	systime = timezone('Asia/Tokyo')
-	sysregion = 'JP'
-	return "東京"
+    global systime, sysregion
+    systime = timezone('Asia/Tokyo')
+    sysregion = 'JP'
+    return "東京"
 
 def set_timeRegion_AUQLD():
-	global systime, sysregion
-	systime = timezone('Australia/Brisbane')
-	sysregion = 'AU/QLD'
-	return "布里斯本"
+    global systime, sysregion
+    systime = timezone('Australia/Brisbane')
+    sysregion = 'AU/QLD'
+    return "布里斯本"
 
 def set_timeRegion_AUMEL():
-	global systime, sysregion
-	systime = timezone('Australia/Melbourne')
-	sysregion = 'AU/MEL'
-	return "墨爾本"
+    global systime, sysregion
+    systime = timezone('Australia/Melbourne')
+    sysregion = 'AU/MEL'
+    return "墨爾本"
 
 def switchRegion(argu):
     switcher = {
         "tw": set_timeRegion_TW,
         "jp": set_timeRegion_JP,
         "qld": set_timeRegion_AUQLD,
-		"mel": set_timeRegion_AUMEL
+        "mel": set_timeRegion_AUMEL
     }
-	return switcher.get(argu, lambda: "Invalid")
+    return switcher.get(argu, lambda: "Invalid")
 
 # english characters have been lower cased. Refer handle_message(event):
-def	onPlayerTalk(user_message, event):
-	global mode, has_said, systime, sysregion, fmt
-	time = str(datetime.now(systime).strftime(fmt))
-	(Hr, Mn) = time.split(':')
-	# Sc = str(datetime.time.second)
-	# only process message when mode = 1;
-	if(mode == 1):
-		if(user_message == "報時" or user_message == "time"):
-			reply_message = "真棒 " + hour_Convert(int(Hr)) + " 時 " + Mn + " 分("+sysregion+")了！！"
-			message = TextSendMessage(text = reply_message)
-			line_bot_api.reply_message(event.reply_token, message)
+def    onPlayerTalk(user_message, event):
+    global mode, has_said, systime, sysregion, fmt
+    time = str(datetime.now(systime).strftime(fmt))
+    (Hr, Mn) = time.split(':')
+    # Sc = str(datetime.time.second)
+    # only process message when mode = 1;
+    if(mode == 1):
+        if(user_message == "報時" or user_message == "time"):
+            reply_message = "真棒 " + hour_Convert(int(Hr)) + " 時 " + Mn + " 分("+sysregion+")了！！"
+            message = TextSendMessage(text = reply_message)
+            line_bot_api.reply_message(event.reply_token, message)
 
-		elif(user_message.find("timezone ") == 0):
-			result = switchRegion( user_message.lstrip("timezone ") )
-			reply_message = "NULL" #declare
-			if(result == "Invalid"):
-				reply_message = ("目前沒有提供此時區選項！")
-			else:
-				reply_message = ("棒棒 系統時間已被設定為 " + result + " 時區.")
-			message = TextSendMessage(text = reply_message)
-			line_bot_api.reply_message(event.reply_token, message)
-			
-		if(int(Mn) == 0 and has_said == 0):
-			has_said = 1
-			reply_message = "好棒 " + hour_Convert(int(Hr)) + " 點了～"
-			message = TextSendMessage(text = reply_message)
-			line_bot_api.reply_message(event.reply_token, message)
-	
-	# reset said @ minute = 30 prevent spam
-	if(int(Mn) >= 3 and has_said == 1):
-		has_said = 0
-	
+        elif(user_message.find("timezone ") == 0):
+            result = switchRegion( user_message.lstrip("timezone ") )
+            reply_message = "NULL" #declare
+            if(result == "Invalid"):
+                reply_message = ("目前沒有提供此時區選項！")
+            else:
+                reply_message = ("棒棒 系統時間已被設定為 " + result + " 時區.")
+            message = TextSendMessage(text = reply_message)
+            line_bot_api.reply_message(event.reply_token, message)
+            
+        if(int(Mn) == 0 and has_said == 0):
+            has_said = 1
+            reply_message = "好棒 " + hour_Convert(int(Hr)) + " 點了～"
+            message = TextSendMessage(text = reply_message)
+            line_bot_api.reply_message(event.reply_token, message)
+    
+    # reset said @ minute = 30 prevent spam
+    if(int(Mn) >= 3 and has_said == 1):
+        has_said = 0
+    
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
