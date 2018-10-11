66846f53fcea5468fd48f65b34a7ef0ca42a8605
Nick Pang <nick95927@gmail.com>
2018-10-11 15:46:24 +1000

    new time zone function

--- app.py
+++ app.py
@@ -135,8 +135,7 @@ def switchRegion(argu):
 		"mel": set_timeRegion_AUMEL
     }
 	return switcher.get(argu, lambda: "Invalid")
-	#return func()
-			
+
 # english characters have been lower cased. Refer handle_message(event):
 def	onPlayerTalk(user_message, event):
 	global mode, has_said, systime, sysregion, fmt
@@ -156,7 +155,7 @@ def	onPlayerTalk(user_message, event):
 			if(result == "Invalid"):
 				reply_message = ("目前沒有提供此時區選項！")
 			else:
-				reply_message = ("系統時間已被設定為 " + result + " 時區.")
+				reply_message = ("棒棒 系統時間已被設定為 " + result + " 時區.")
 			message = TextSendMessage(text = reply_message)
 			line_bot_api.reply_message(event.reply_token, message)
 			
